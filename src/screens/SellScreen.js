9b24b1c5a847616dc2c9dbb21783c7d3997f2bb6
Carolyn Perniciaro <cmpvm5@mail.missouri.edu>
2021-02-04 21:24:42 -0600

    sad carol :( --added upload button

--- src/screens/SellScreen.js
+++ src/screens/SellScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, Text, TouchableOpacity } from 'react-native';
 import FormField from '../components/SellScreen/FormField'
+import { Icon } from 'react-native-elements'
 
 const SellScreen = ({ navigation }) => {
    
     return (
         <View style={styles.container1}>
-            <FormField label={'Your Full Name'} placeholder={'First, Last'}/>
-            <FormField label={'Your Email'} placeholder={'YourEmail@you.edu'}/>
+            <FormField label={'Your Full Name'} placeholder={'First Last'}/>
+            <FormField label={'Your Email'} placeholder={'example@you.edu'}/>
             <FormField label={'Book Title'} placeholder={'Title'}/>
             <FormField label={'Book Author'} placeholder={'Author'}/>
             <FormField label={'ISBN'} placeholder={'ISBN'}/>
             <FormField label={'Book Price'} placeholder={'Price'}/>
-            <FormField label={'LOCATION?'} placeholder={''}/>
-            <FormField label={'PIC_UPLOAD'} placeholder={''}/>
+            <FormField label={'Location'} placeholder={'Pick-up Location'}/>
+            {/* <FormField label={'PIC_UPLOAD'} placeholder={''}/> */}
+            <Text>
+                Book Image
+            </Text>
+            <TouchableOpacity style={styles.picButton}> 
+                <Icon
+                    name='camera'
+                    type='font-awesome'
+                    color='#f50'
+                />
+            </TouchableOpacity>
         </View>
     );
 };
@@ -36,6 +47,21 @@ const styles = StyleSheet.create({
     errorMsg: {
         marginBottom: 20,
         height: 17.5,
+    },
+    picButton: {
+        width: 70,
+        height: 70,
+        backgroundColor: 'white',
+        borderRadius: 50,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 });
 
